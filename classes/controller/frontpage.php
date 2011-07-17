73f2ab534c8b5c90c8444f1615ec024bf0bd91b6
Gabriel Reitz Giannattasio <gabriel@loadin.com.br>
2011-07-16 22:03:52 -0300

    Added printable action (closes #82)

--- classes/controller/frontpage.php
+++ classes/controller/frontpage.php
@@ -2,12 +2,45 @@
 
 class Controller_Frontpage extends Controller {
 
+	/**
+	 * @var boolean  execute default template
+	 */
+	protected $auto_template = TRUE;
+
+	/**
+	 * @var string  default template string
+	 */
+	protected $template;
+
+	public function before()
+	{
+		// Exit if auto_template is disabled
+		if( ! $this->auto_template) return;
+
+		// Set the default template file
+		$this->template = View::factory('html/dynamic/template')
+			->set('content', NULL);
+	}
+
+	public function after()
+	{
+		// Exit if auto_template is disabled
+		if( ! $this->auto_template) return;
+
+		$this->response->body($this->template);
+	}
+
 	public function action_index()
 	{
+		// Load dynamic menu and static curriculum into template
+		$this->template->set('content',View::factory('html/static/2011-1_curriculum_english_resume'));
+		
+	}
+
+	public function action_printable()
+	{
 		// Load static curriculum into template
-		$template = View::factory('html/dynamic/template')
-			->set('content',View::factory('html/static/2011-1_curriculum_english_resume'));
-		$this->response->body($template);
+		$this->template->set('content',View::factory('html/static/2011-1_curriculum_english_resume'));
 	}
 
 } // End Frontpage
